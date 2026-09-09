102b618679e8025e94373b39705700ac6124bfac
Devansh Ajmera <devanshajmera1012@gmail.com>
2026-09-10 00:23:44 +0530

    input added

--- Practical_2/approximation_vc.py
+++ Practical_2/approximation_vc.py
@@ -1,9 +1,10 @@
 def maximal_matching(edges):
     match = []
     vc = []
-
-    vc.append(edges[0][0])
-    vc.append(edges[0][1])
+    u,v = edges[0][0], edges[0][1]
+    vc.append(u)
+    vc.append(v)
+    match.append((u,v))
 
     for u,v in edges:
         if u not in vc and v not in vc:
@@ -11,4 +12,24 @@ def maximal_matching(edges):
             vc.append(v)
             match.append((u,v))
 
-    return match
+    return vc, match
+
+
+
+def read_input(i):
+    edges = []
+    with open(f'Input/input{i}.txt', 'r') as f:
+        lines = f.readlines()
+        n, m = map(int, lines[0].split())
+
+        for line in lines[1:]:
+            if not line:
+                continue
+            u, v = map(int, line.split())
+            edges.append((u,v))
+    return n, m, edges
+
+n, m, edges = read_input(1)
+vc, match = maximal_matching(edges)
+print(n, m, edges)
+print(vc, len(vc), match)
