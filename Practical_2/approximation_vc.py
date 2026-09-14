dd63e577833c5bd04492b8f8fa92d6469bc0bcff
Devansh Ajmera <devanshajmera1012@gmail.com>
2026-09-14 23:34:08 +0530

    store in csv

--- Practical_2/approximation_vc.py
+++ Practical_2/approximation_vc.py
@@ -1,11 +1,9 @@
 import time
+import csv
+
 def maximal_matching(edges):
     match = []
     vc = []
-    u,v = edges[0][0], edges[0][1]
-    vc.append(u)
-    vc.append(v)
-    match.append((u,v))
 
     for u,v in edges:
         if u not in vc and v not in vc:
@@ -30,13 +28,41 @@ def read_input(i):
             edges.append((u,v))
     return n, m, edges
 
+def csv_update(rows, n, m, vc, size, time):
+    for row in rows[1:]:
+        if row[0] == str((n,m)):
+            row[4] = vc
+            row[5] = size
+            row[6] = time
+            break
 
 
-for i in range(1,9):
-    n, m, edges = read_input(i)
-    st_time = time.perf_counter()
-    vc, match = maximal_matching(edges)
-    end_time = time.perf_counter()
-    print('Vertex cover:',vc,'\n','size:',len(vc),'\n','Maximal_matching',match,)
-    print('Time',(end_time-st_time)*1000)
-    print('\n')
+
+def main():
+    with open('../Practical_1/result.csv','r',newline='') as f:
+            rows = list(csv.reader(f))
+    
+    if 'Approximation_vc' not in rows[0]:
+        rows[0].extend(['Approximation_vc', 'Size', 'Execution_Time'])
+    
+        for row in rows[1:]:
+                row.extend(['', '', ''])
+
+    for i in range(1,9):
+        n, m, edges = read_input(i)
+        
+        st_time = time.perf_counter()
+        vc, match = maximal_matching(edges)
+        end_time = time.perf_counter()
+
+        execution_time = (end_time-st_time)*1000
+
+        csv_update(rows, n,m,vc, len(vc), execution_time)
+
+    with open('../Practical_1/result.csv', 'w', newline='') as f:
+        w = csv.writer(f)
+        w.writerows(rows)
+
+
+if __name__ == '__main__':
+    main()
