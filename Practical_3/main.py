2b98f7e6c71bd3cc29039d2318cc4b6e41bb1b6e
Devansh Ajmera <devanshajmera1012@gmail.com>
2026-09-22 21:49:05 +0530

    updated

--- Practical_3/main.py
+++ Practical_3/main.py
@@ -21,47 +21,6 @@ def generate_edges(n, m):
         edges.add(edge)
     return list(edges)
 
-def subsets(index,subset,vertices,powerset):
-    if index >= len(vertices):
-        powerset.append(subset.copy())
-        return 
-
-    subset.append(vertices[index])
-    subsets(index+1,subset,vertices,powerset)
-    subset.pop()
-    subsets(index+1,subset,vertices,powerset)
-
-def isvc(g,subset):
-    for u,v in g.edges():
-        if u not in subset and v not in subset:
-            return False
-    return True
-
-def min_vc(g,vertices,powerset):
-    min = len(vertices)
-    vc = vertices
-    for subset in powerset:
-        if isvc(g,subset) and min > len(subset):
-            min = len(subset)
-            vc = subset
-    return vc
-
-
-def approximation_algo(edges):
-    match = set()
-    vc = set()
-
-    for u, v in edges:
-        if u not in vc and v not in vc:
-            match.add((u, v))
-            vc.add(u)
-            vc.add(v)
-
-    return vc, match
-
-def approximation_factor(vc, opt_size):
-    return round(len(vc) / opt_size,5)
-
 def write_edges(n, m, edges,i):
     with open(f'Input/input{i}.txt', "w") as f:
         f.write(f"{n} {m}\n")
@@ -81,46 +40,76 @@ def read_edges(i):
         edges.append((u, v))
     return edges
 
+def subsets(index,subset,vertices,powerset):
+    if index >= len(vertices):
+        powerset.append(subset.copy())
+        return 
+
+    subset.append(vertices[index])
+    subsets(index+1,subset,vertices,powerset)
+    subset.pop()
+    subsets(index+1,subset,vertices,powerset)
+
+def isvc(g,subset):
+    for u,v in g.edges():
+        if u not in subset and v not in subset:
+            return False
+    return True
+
+def min_vc(g,vertices,powerset):
+    min_size = len(vertices)
+    vc = vertices
+    for subset in powerset:
+        if isvc(g,subset) and min_size > len(subset):
+            min_size = len(subset)
+            vc = subset
+    return vc
+
+
+def approximation_algo(edges):
+    match = set()
+    vc = set()
+
+    for u, v in edges:
+        if u not in vc and v not in vc:
+            match.add((u, v))
+            vc.add(u)
+            vc.add(v)
+
+    return vc, match
+
+def approximation_factor(vc, opt_size):
+    return round(len(vc) / opt_size,5)
+
 def brute_force_display(n, m, edges, i):
     os.makedirs('Visualize_brute',exist_ok=True)
 
+    vertices = list(range(1,n+1))
     G = nx.Graph()
-    vertices = [i+1 for i in range(n)]
     G.add_nodes_from(vertices)
     G.add_edges_from(edges)
 
     st_time = time.perf_counter()
-
     powerset = []
     subsets(0,[],vertices,powerset)
     vc = min_vc(G,vertices,powerset)
-
     end_time = time.perf_counter()
 
-    execution_time = (end_time-st_time)*1000
-
-    with open('result.csv', 'a', newline='') as f:
-        w = csv.writer(f)
-        w.writerow([(n,m),len(vc),execution_time])
-
-
-    # color = []
-    # for v in vertices:
-    #     if v in vc:
-    #         color.append('red')
-    #     else:
-    #         color.append('blue')
+    execution_time = (end_time-st_time)
+    color = []
+    for v in vertices:
+        if v in vc:
+            color.append('red')
+        else:
+            color.append('lightblue')
     
-    # plt.figure(figsize=(8, 6))
-    # coordinates = nx.circular_layout(G)
-    # plt.title(f"Graph {i} = {n,m}")
-    # nx.draw(
-    #     G,coordinates,
-    #     with_labels=True,node_color=color, node_size=600,
-    # )
-    # plt.savefig(f'Visualize_brute/graph{i}.png', dpi=300)
-
-    # return len(vc)
+    plt.figure(figsize=(10, 8))
+    plt.title(f"Graph {i} = {n,m}")
+    coordinates = nx.circular_layout(G)
+    nx.draw(G, coordinates, with_labels=True,node_color=color, node_size=600)
+    plt.savefig(f'Visualize_brute/graph{i}.png', dpi=300)
+    plt.close()
+    return len(vc), execution_time
 
 def approximation_display(n,m,edges,opt_size,i):
     os.makedirs('Visualize_approx',exist_ok=True)
@@ -128,10 +117,9 @@ def approximation_display(n,m,edges,opt_size,i):
     st_time = time.perf_counter()
     vc, match = approximation_algo(edges)
     end_time = time.perf_counter()
-    execution_time = (end_time-st_time)/1000
+    execution_time = (end_time-st_time)
 
     factor = approximation_factor(vc, opt_size)
-
     G = nx.Graph()
     G.add_nodes_from(range(1, n + 1))
     G.add_edges_from(edges)
@@ -142,7 +130,6 @@ def approximation_display(n,m,edges,opt_size,i):
             edge_color.append('red')
         else:
             edge_color.append('black')
-
     node_color = []
     for v in G.nodes():
         if v in vc:
@@ -151,29 +138,33 @@ def approximation_display(n,m,edges,opt_size,i):
             node_color.append('lightblue')
 
     plt.figure(figsize=(10, 8))
-    # pos = nx.circular_layout(G)
     plt.title(f'G{i} = ({n,m})')
-    nx.draw(G, with_labels=True, node_color=node_color, edge_color=edge_color,node_size=700)
+    coordinates = nx.circular_layout(G)
+    nx.draw(G, coordinates, with_labels=True, node_color=node_color, edge_color=edge_color, edgelist= edges, node_size=700)
 
-    output_file = os.path.join('Visualize_approx', f"graph_{i}.png")
-    plt.savefig(output_file,dpi=300)
+    plt.savefig(f'Visualize_approx/graph{i}.png', dpi=300)
+    plt.close()
+    return len(vc), execution_time, len(match), factor
 
 def main():
     os.makedirs('Input',exist_ok=True)
 
     with open('result.csv', 'w', newline ='') as f:
         w = csv.writer(f)
-        w.writerow(['(Vertex,Edge)','Size','Execution Time', 'vc_size(approx.)', 'Execution_Time', 'Match_size', 'Approximation_Factor'])
+        w.writerow(['(Vertex,Edge)','Optimal_Size','Brute_Force_Time', 'Approx_Size', 'Approx_Time', 'Match_size', 'Approximation_Factor'])
 
     n = 20
     i = 1
-    for m in range(20,190,10):
-        # edges = generate_edges(n, m)
-        # write_edges(n, m, edges,i)
+    for m in (20,40,60,80,100,120,140,160,180,190):
+        edges = generate_edges(n, m)
+        write_edges(n, m, edges,i)
         edges = read_edges(i)
-        opt_size = brute_force_display(n, m, edges, i)
-        # approximation_display(n,m,edges,opt_size, i)
-        i += 1
+        opt_size, brute_time = brute_force_display(n, m, edges, i)
+        approx_size, approx_time, match_size, factor = approximation_display(n,m,edges,opt_size, i)
 
-if __name__ == '__main__':
-    main()
+        with open('result.csv', 'a', newline='') as f:
+            w = csv.writer(f)
+            w.writerow([(n,m), opt_size, brute_time, approx_size, approx_time, match_size, factor])
+        print(f'Output {i} saved')
+        i += 1
+main()
